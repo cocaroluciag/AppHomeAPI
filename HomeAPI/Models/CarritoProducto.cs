8da545b80ec5ce1b759d0f03f7ff112c8f040631
cocaroluciag <cocaroluciaguadalupe@gmail.com>
2024-09-27 03:04:17 -0300

    Actualizar clases Producto y CarritoProducto

--- HomeAPI/Models/CarritoProducto.cs
+++ HomeAPI/Models/CarritoProducto.cs
@@ -1,6 +1,17 @@
-﻿namespace HomeAPI.Models
+﻿using System;
+using System.Collections.Generic;
+
+namespace HomeAPI.Models
 {
     public class CarritoProducto
     {
+        public int IdCarritoProducto { get; set; } 
+        public int IdCarrito { get; set; } 
+        public int IdProducto { get; set; } 
+        public int Cantidad { get; set; } 
+
+        // Relaciones
+        public virtual Carrito Carrito { get; set; } // Llave foránea a Carrito
+        public virtual Producto Producto { get; set; } // Llave foránea a Producto
     }
 }
--- HomeAPI/Models/Producto.cs
+++ HomeAPI/Models/Producto.cs
@@ -1,6 +1,20 @@
-﻿namespace HomeAPI.Models
+﻿using System;
+using System.Collections.Generic;
+
+namespace HomeAPI.Models
 {
     public class Producto
     {
+        public int IdProducto { get; set; }
+        public string NombreProducto { get; set; }
+        public string Descripcion { get; set; }
+        public decimal Precio { get; set; } 
+        public int Stock { get; set; } 
+        public string Categoria { get; set; } 
+        public string Imagen { get; set; } 
+
+        // Relación con CarritoProducto
+        public virtual ICollection<CarritoProducto> CarritoProductos { get; set; }
+
     }
 }
