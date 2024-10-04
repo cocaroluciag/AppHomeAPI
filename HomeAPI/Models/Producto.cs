8e04f149d46669fa40a762937d56d39bb3578c24
cocaroluciag <cocaroluciaguadalupe@gmail.com>
2024-10-04 00:50:11 -0300

    Actualización de Producto.cs

--- HomeAPI/Models/Producto.cs
+++ HomeAPI/Models/Producto.cs
@@ -1,20 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace HomeAPI.Models
 {
     public class Producto
     {
+        [Key]
         public int IdProducto { get; set; }
+
+        [Required]
+        [StringLength(100)]
         public string NombreProducto { get; set; }
+
+        [StringLength(500)]
         public string Descripcion { get; set; }
-        public decimal Precio { get; set; } 
-        public int Stock { get; set; } 
-        public string Categoria { get; set; } 
-        public string Imagen { get; set; } 
 
-        // Relación con CarritoProducto
+        [Required]
+        [Range(0, double.MaxValue)]
+        public decimal Precio { get; set; }
+
+        [Required]
+        [Range(0, int.MaxValue)]
+        public int Stock { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Categoria { get; set; }
+
+        [StringLength(500)]
+        public string Imagen { get; set; }
+
+        // Relación con CarritoProducto (uno a muchos)
         public virtual ICollection<CarritoProducto> CarritoProductos { get; set; }
-
     }
 }
