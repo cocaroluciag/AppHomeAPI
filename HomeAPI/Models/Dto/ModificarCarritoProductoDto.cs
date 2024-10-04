2a8048bdd62230ffc36fa395cf51bde4c0078e53
cocaroluciag <cocaroluciaguadalupe@gmail.com>
2024-10-04 01:14:04 -0300

    Creación del Dto ModificarCarritoProducto

---
+++ HomeAPI/Models/Dto/ModificarCarritoProductoDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HomeAPI.Models.Dto
+{
+    public class ModificarCarritoProductoDto
+    {
+        [Required]
+        [Range(1, int.MaxValue)]
+        public int Cantidad { get; set; }
+    }
+}
