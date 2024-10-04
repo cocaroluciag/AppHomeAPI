b75eedb5e205656c52ea82c4e14ec420bb76861b
cocaroluciag <cocaroluciaguadalupe@gmail.com>
2024-10-04 01:10:38 -0300

    Creación del Dto ModificarProducto

---
+++ HomeAPI/Models/Dto/ModificarProductoDto.cs
@@ -0,0 +1,12 @@
+﻿namespace HomeAPI.Models.Dto
+{
+    public class ModificarProductoDto
+    {          
+        public string NombreProducto { get; set; }    
+        public string Descripcion { get; set; }        
+        public decimal Precio { get; set; }             
+        public int Stock { get; set; }                 
+        public string Categoria { get; set; }           
+        public string Imagen { get; set; }
+    }
+}
