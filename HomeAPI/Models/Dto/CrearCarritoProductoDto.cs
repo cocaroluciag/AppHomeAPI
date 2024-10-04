7922000873ce65687aae1c9430e04cf638323f0f
cocaroluciag <cocaroluciaguadalupe@gmail.com>
2024-10-04 01:13:00 -0300

    Creación del Dto CrearCarritoProducto

---
+++ HomeAPI/Models/Dto/CrearCarritoProductoDto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HomeAPI.Models.Dto
+{
+    public class CrearCarritoProductoDto
+    {
+       // [Required]
+        public int IdCarrito { get; set; }
+
+        [Required]
+        public int IdProducto { get; set; }
+
+        [Required]
+        [Range(1, int.MaxValue)]
+        public int Cantidad { get; set; }
+    }
+}
